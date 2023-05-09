a7edc00f5f2aac37c86747a47ae36a30eb028ea0
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-05-09 21:46:10 +0200

    Setup express and MongoDB connection

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import  express  from "express";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectToDB from "./mongodb/connect.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb'}));
+
+app.get('/', async(req, res) => {
+    res.send('Hello folks!')
+})
+
+const startServer = async () => {
+
+    try {
+        connectToDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log('Server active on http://localhost:8080'))
+    } catch (error) {
+        console.log(error);
+    }
+
+    
+}
+
+startServer();
---
+++ server/mongodb/connect.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectToDB = (url) => {
+    mongoose.set('strictQuery', true)
+
+    mongoose.connect(url)
+        .then(() => console.log('Connected to MongoDB'))
+        .catch((err) => console.log(err))
+}
+
+export default connectToDB;
